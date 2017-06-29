ed811a666b3d5840c366b5e6c26bbf95855df126
amrithadevi <sangeethameganathan98@gmail.com>
2017-06-29 13:55:51 +0530

    Create aaa.c

---
+++ aaa.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+  if(a!=0)
+ printf("positive");
+  else
+  printf("negative");
+  }
+  else
+  printf("zero");
+  getch();
+  }
